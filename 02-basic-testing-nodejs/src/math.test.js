bd32844b7e4ed718c65c58c6f9da5eee3ffbb27d
Sumit Paul <pauls1317@gmail.com>
2023-10-14 01:01:15 +0530

    copied tests for backend projects

---
+++ 02-basic-testing-nodejs/src/math.test.js
@@ -0,0 +1,59 @@
+import { expect, it } from 'vitest';
+import { add } from './math';
+
+it('should sum all number values in array', () => {
+  // Arrange
+  const numbers = [1, 2, 3];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce(
+    (prevValue, currValue) => prevValue + currValue
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN', () => {
+  const inputs = ['invalid', 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it('empty array', () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+// .not property
+it('not an error', () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).not.toBeNaN();
+});
+
+// toThrow
+it('error test', () => {
+  const resultFn = () => {
+    add(); // not passing any argument
+  };
+
+  expect(resultFn).toThrow();
+});
+
+it('multiple arguments', () => {
+  const num1 = 1;
+  const num2 = 2;
+  const resultFn = () => {
+    add(num1, num2);
+  };
+  expect(resultFn).toThrow(/is not iterable/);
+});
---
+++ 02-basic-testing-nodejs/src/util/numbers.test.js
@@ -0,0 +1,20 @@
+import { expect, test } from 'vitest';
+import { transformToNumber } from './numbers';
+
+test('string number to number', () => {
+  const input = '12';
+  const result = transformToNumber(input);
+
+  expect(result).toBeTypeOf('number');
+});
+
+test('yeild NaN', () => {
+  const input = 'something';
+  const input2 = {};
+
+  const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
+
+  expect(result).toBeNaN();
+  expect(result2).toBeNaN();
+});
